fb9ad0bcb9b4b8c7796a215ab0b73b7821a26a0e
Richard Gobert <dev.richard.g@gmail.com>
2019-09-28 18:43:42 +0200

    add variable js buttom, li and switch picture

--- script.js
+++ script.js
@@ -1,2 +1,7 @@
 "use strict";
 
+const Previous = document.querySelector(".btn-previous");
+const next = document.querySelector(".btn-next");
+let li = document.querySelectorAll("li");
+let slideNext = 0;
+let sildePrevious = li.length;
